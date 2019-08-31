ccd8dd1555354d238a683e68c053b3ff7f3605d5
woodpav <devwoodpav@gmail.com>
2019-08-31 09:49:16 -0700

    [0.0.5] Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/woodpav/tal",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["click"],
